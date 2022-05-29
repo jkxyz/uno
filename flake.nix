d65dbe87d93c9a60e38e767268394c506d2427a8
Josh Kingsley <josh@joshkingsley.me>
2022-05-29 22:01:36 +0300

    Add basic derivation

--- flake.nix
+++ flake.nix
@@ -1,8 +1,28 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }: {
-    unoConfigurations.default = {};
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        unoConfigurations.default = self.lib.mkUnoConfiguration {
+          inherit system;
+          services.postgres = { runner = "${pkgs.postgresql}/bin/postgres"; };
+        };
+      }) // {
+        lib = {
+          mkUnoConfiguration = { system, services }:
+            let
+              pkgs = import nixpkgs { inherit system; };
+              procfile = pkgs.writeText "Procfile"
+                (builtins.concatStringsSep "\n" (builtins.attrValues
+                  (builtins.mapAttrs (name: value: "${name}: ${value.runner}")
+                    services)));
+            in pkgs.writers.writeBashBin "uno" ''
+              exec ${pkgs.foreman}/bin/foreman $@ --procfile ${procfile}
+            '';
+        };
+      };
 }
