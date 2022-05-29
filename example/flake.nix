fd69c477f623639869e5f4fa2dce6479db69fdd8
Josh Kingsley <josh@joshkingsley.me>
2022-05-29 22:13:24 +0300

    Extract example to separate flake

---
+++ example/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    uno.url = "path:..";
+    uno.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, uno }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        unoConfigurations.default = uno.lib.mkUnoConfiguration {
+          inherit system;
+          services.postgres = { runner = "${pkgs.postgresql}/bin/postgres"; };
+        };
+      });
+}
--- flake.nix
+++ flake.nix
@@ -1,28 +1,18 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+
+  outputs = { self, nixpkgs }: {
+    lib = {
+      mkUnoConfiguration = { system, services }:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          procfile = pkgs.writeText "Procfile" (builtins.concatStringsSep "\n"
+            (builtins.attrValues
+              (builtins.mapAttrs (name: value: "${name}: ${value.runner}")
+                services)));
+        in pkgs.writers.writeBashBin "uno" ''
+          exec ${pkgs.foreman}/bin/foreman $@ --procfile ${procfile}
+        '';
+    };
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        unoConfigurations.default = self.lib.mkUnoConfiguration {
-          inherit system;
-          services.postgres = { runner = "${pkgs.postgresql}/bin/postgres"; };
-        };
-      }) // {
-        lib = {
-          mkUnoConfiguration = { system, services }:
-            let
-              pkgs = import nixpkgs { inherit system; };
-              procfile = pkgs.writeText "Procfile"
-                (builtins.concatStringsSep "\n" (builtins.attrValues
-                  (builtins.mapAttrs (name: value: "${name}: ${value.runner}")
-                    services)));
-            in pkgs.writers.writeBashBin "uno" ''
-              exec ${pkgs.foreman}/bin/foreman $@ --procfile ${procfile}
-            '';
-        };
-      };
 }
