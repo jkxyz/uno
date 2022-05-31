94cca5433fd196f5bceceba4bf4847f53805653a
Josh Kingsley <josh@joshkingsley.me>
2022-05-31 23:09:32 +0300

    Allow UNO_ROOT to change path to flake.nix

--- uno.sh
+++ uno.sh
@@ -1,10 +1,11 @@
 ACTION="$1"
 SYSTEM=$(nix eval --impure --expr "builtins.currentSystem")
 CONFIG_NAME="${2:-default}"
-FLAKE=".#unoConfigurations.$SYSTEM.$CONFIG_NAME.foremanWrapper"
+ROOT=${UNO_ROOT-$PWD}
+FLAKE="path:$ROOT#unoConfigurations.$SYSTEM.$CONFIG_NAME.foremanWrapper"
 
 nix build --no-link $FLAKE
 
 FOREMAN_WRAPPER="$(nix path-info $FLAKE)/bin/uno-foreman-wrapper"
 
-$FOREMAN_WRAPPER $ACTION
+$FOREMAN_WRAPPER $ACTION --root=${UNO_ROOT-$PWD}
