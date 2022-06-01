3694b954732dc751d90021fccbc6de457fcf8da9
Josh Kingsley <josh@joshkingsley.me>
2022-06-01 16:37:21 +0300

    Ensure symlink is under $ROOT

--- uno.sh
+++ uno.sh
@@ -5,7 +5,7 @@ ROOT=${UNO_ROOT-$PWD}
 CONFIG_NAME=default
 PROCFILE_URL=path:$ROOT#unoConfigurations.$SYSTEM.$CONFIG_NAME.procfile
 
-nix build --out-link .uno/$CONFIG_NAME/Procfile $PROCFILE_URL
+nix build --out-link $ROOT/.uno/$CONFIG_NAME/Procfile $PROCFILE_URL
 
 PROCFILE=$(nix path-info $PROCFILE_URL)
 
