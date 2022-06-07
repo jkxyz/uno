c0d27456c0737cd8b5a47868f2d17d7e73e525e1
Josh Kingsley <josh@joshkingsley.me>
2022-06-07 20:52:36 +0300

    Add more examples

--- example/flake.nix
+++ example/flake.nix
@@ -13,9 +13,21 @@
         unoConfigurations.default = uno.lib.mkUnoConfiguration {
           inherit system;
 
+          services.echo = {
+            environment = { YOUR_NAME = "World"; };
+            command = "echo Hello, $YOUR_NAME && sleep 5000";
+          };
+
           services.postgres = uno.lib.mkPostgresService {
             inherit system;
             dataDir = "data/postgres";
+          };
+
+          services.postgres13 = uno.lib.mkPostgresService {
+            inherit system;
+            package = pkgs.postgresql_13;
+            dataDir = "data/postgres13";
+            port = 5433;
           };
         };
       });
