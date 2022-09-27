300172df4f1bc1e013aa0396cf5e8c6ac3e6f554
Josh Kingsley <josh@joshkingsley.me>
2022-09-28 00:00:13 +0300

    Add --formation flag

--- uno.sh
+++ uno.sh
@@ -2,6 +2,36 @@
 
 set -e
 
+help() {
+    cat << EOF
+uno - declarative development processes with Nix
+
+Usage:
+
+uno [OPTS] COMMAND [CONFIG]
+
+Options:
+
+    --root DIR
+        Path to the directory containing a flake.nix with
+        an uno configuration. Defaults to current working
+        directory. Can also be set with the UNO_ROOT
+        environment variable.
+
+    -m, --formation process=num[,...]
+        The number of processes to run.
+
+    --offline
+        Passes the --offline flag to nix when building.
+
+Commands:
+
+    start [CONFIG]
+        Starts the specified configuration, or the default
+        configuration if none is provided.
+EOF
+}
+
 ROOT="${UNO_ROOT-$PWD}"
 
 ARGS=()
@@ -17,8 +47,18 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
+        -m|--formation)
+            FORMATION="$2"
+            shift
+            shift
+            ;;
+        --help|-h)
+            help
+            exit 0
+            ;;
         -*|--*)
             echo "Unknown option $1"
+            help
             exit 1
             ;;
         *)
@@ -29,34 +69,6 @@ done
 
 set -- "${ARGS[@]}"
 
-help() {
-    cat << EOF
-uno - declarative development services with Nix
-
-Usage:
-
-uno [OPTS] COMMAND [CONFIG]
-
-Options:
-
-    --root
-        Path to the directory containing a flake.nix with
-        an uno configuration. Defaults to current working
-        directory. Can also be set with the UNO_ROOT
-        environment variable.
-
-    --offline
-        Passes the --offline flag to nix when building.
-
-Commands:
-
-    start [CONFIG]
-        Starts the specified configuration, or the default
-        configuration if none is provided.
-
-EOF
-}
-
 case $1 in
     start)
         CONFIG_NAME="${2-default}"
@@ -64,7 +76,7 @@ case $1 in
         PROCFILE_URL="path:$ROOT#unoConfigurations.$SYSTEM.$CONFIG_NAME.procfile"
         nix build ${OFFLINE:+--offline} --out-link "$ROOT/.uno/$CONFIG_NAME/Procfile" $PROCFILE_URL
         PROCFILE_PATH="$(nix path-info $PROCFILE_URL)"
-        foreman start --root="$ROOT" --procfile="$PROCFILE_PATH"
+        foreman start --root="$ROOT" --procfile="$PROCFILE_PATH" ${FORMATION:+--formation=$FORMATION}
         ;;
     help)
         help
